f91e7cf09f1da65437347a77e541a908ef45256f
DavidIfebueme <davidifebueme@gmail.com>
2023-06-06 18:48:29 +0100

    updates

--- sample_dbms/user_register/models.py
+++ sample_dbms/user_register/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Role(models.Model):
+    title = models.CharField(max_length=300)
+
+class User(models.Model):
+    fullname = models.CharField(max_length=200)
+    emp_code = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=20)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
--- sample_dbms/user_register/views.py
+++ sample_dbms/user_register/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def user_list(request):
+    return
+
+def user_form(request):
+    return
+
+def user_delete(request):
+    return    
